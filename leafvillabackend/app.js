a3dc1ab20181c8d641b05a33c5221b4983dc6599
sambhunarayan <msambhunarayan07@gmail.com>
2024-10-06 19:55:12 +0530

    cors
    -cors added to app.js

--- leafvillabackend/app.js
+++ leafvillabackend/app.js
@@ -8,7 +8,23 @@ require('dotenv').config();
 var log4js = require('log4js');
 var log = log4js.getLogger();
 log4js.configure('./log.json');
-
+const cors = require('cors');
+const corsOptions = {
+	origin: (origin, callback) => {
+		const whitelist = ['http://localhost:3000', '*'];
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	allowedHeaders: 'Content-Type, Authorization',
+	exposedHeaders: 'Content-Length, X-Requested-With',
+	credentials: true,
+	optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
